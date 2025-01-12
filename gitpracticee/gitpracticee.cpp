e0f47deeed4841e81d9dd5d52ffe23dae75f45a1
hira-O <hiranaseer1581@gmail.com>
2025-01-12 18:52:13 +0500

    Add project files.

---
+++ gitpracticee/gitpracticee.cpp
@@ -0,0 +1,28 @@
+// gitpracticee.cpp :
+
+#include <iostream>
+using namespace std;
+
+int sum(int a, int b) {
+    return a + b;
+}
+
+int sub(int a, int b) {
+    return a - b;
+}
+
+int main()
+{
+    int a, b;
+    cout << "Enter a number a: " << endl;
+    cin >> a;
+    cout << "Enter a number b: " << endl;
+    cin >> b;
+
+    cout << "sum of a and b is : " << sum(a, b) << endl;
+    cout << "subtraction of a and b is : " << sub(a, b) << endl;
+    return 0;
+    
+}
+
+
